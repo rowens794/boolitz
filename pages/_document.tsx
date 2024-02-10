0e2589ac943febb09ecf832c3746568ba2f7aa2b
rowens794 <rowens794@gmail.com>
2024-02-10 14:14:42 -0500

    tiny analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,10 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap"
           rel="stylesheet"
         />
+        <script
+          defer
+          src="https://app.tinyanalytics.io/pixel/5sn0rILLGfZ35XsW"
+        ></script>
       </Head>
       <body className="bg-gray-50">
         <Main />
